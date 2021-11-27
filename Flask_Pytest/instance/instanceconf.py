81161f971bd221f2e6ad6b1dcafcce57b8f2f632
grace <clineg21@gmail.com>
2021-11-26 21:18:28 -0500

    changed database url

--- Flask_Pytest/instance/instanceconf.py
+++ Flask_Pytest/instance/instanceconf.py
@@ -3,7 +3,8 @@ from decouple import config
 class Config:
     DEBUG=False
     CSRF_ENABLED=True
-    SQLALCHEMY_DATABASE_URI=config('DATABASE_URL')
+    # SQLALCHEMY_DATABASE_URI=config('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI='DATABASE_URL'
     SQLALCHEMY_TRACK_MODIFICTIONS=False
 
 class DevelopmentConfig(Config):
@@ -11,7 +12,8 @@ class DevelopmentConfig(Config):
 
 class TestingConfig(Config):
     TESTING=True
-    SQLALCHEMY_DATABASE_URI=config('DATABASE_TEST_URL')
+    # SQLALCHEMY_DATABASE_URI=config('DATABASE_TEST_URL')
+    SQLALCHEMY_DATABASE_URI='HEROKU_POSTGRESQL_GRAY_URL'
     # DEBUG=True
 
 app_config = {
