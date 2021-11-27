78243be61123a1cfb307dcc8432efa48375dbc74
grace <clineg21@gmail.com>
2021-11-26 21:23:07 -0500

    WIP changed database uri def

--- Flask_Pytest/instance/instanceconf.py
+++ Flask_Pytest/instance/instanceconf.py
@@ -1,10 +1,13 @@
 from decouple import config
+import os
+import psycopg2
 
 class Config:
     DEBUG=False
     CSRF_ENABLED=True
     # SQLALCHEMY_DATABASE_URI=config('DATABASE_URL')
-    SQLALCHEMY_DATABASE_URI='DATABASE_URL'
+    SQLALCHEMY_DATABASE_URI=os.environ['DATABASE_URL']
+    conn = psycopg2.connect(SQLALCHEMY_DATABASE_URI, sslmode='require')
     SQLALCHEMY_TRACK_MODIFICTIONS=False
 
 class DevelopmentConfig(Config):
@@ -13,7 +16,9 @@ class DevelopmentConfig(Config):
 class TestingConfig(Config):
     TESTING=True
     # SQLALCHEMY_DATABASE_URI=config('DATABASE_TEST_URL')
-    SQLALCHEMY_DATABASE_URI='HEROKU_POSTGRESQL_GRAY_URL'
+    # SQLALCHEMY_DATABASE_URI='HEROKU_POSTGRESQL_GRAY_URL'
+    SQLALCHEMY_DATABASE_TEST_URI = os.environ['HEROKU_POSTGRESQL_GRAY_URL']
+    conn = psycopg2.connect(SQLALCHEMY_DATABASE_TEST_URI, sslmode='require')
     # DEBUG=True
 
 app_config = {
